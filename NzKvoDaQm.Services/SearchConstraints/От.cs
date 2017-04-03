2982b635f4de3abd417d0a50a3a695a1a82c7ddf
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-04-03 17:35:33 +0300

    Constraint allowing only recipes from specific authors

---
+++ NzKvoDaQm.Services/SearchConstraints/От.cs
@@ -0,0 +1,30 @@
+﻿namespace NzKvoDaQm.Services.SearchConstraints
+{
+
+    using System;
+
+    using NzKvoDaQm.Models.EntityModels;
+
+    public class От : ISearchConstraint
+    {
+        private readonly string authorUsername;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public От(string authorUsername)
+        {
+            if (string.IsNullOrWhiteSpace(authorUsername))
+            {
+                throw new ArgumentNullException("authorUsername");
+            }
+
+            this.authorUsername = authorUsername.Trim();
+        }
+
+        public bool IsAllowed(Recipe recipe)
+        {
+            return recipe.Author.UserName == this.authorUsername;
+        }
+    }
+}
---
+++ NzKvoDaQm.Tests/Services/SearchService/Constraints/ОтTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NzKvoDaQm.Tests.Services.SearchService.Constraints
+{
+
+    using NzKvoDaQm.Models.EntityModels;
+    using NzKvoDaQm.Models.ViewModels;
+    using NzKvoDaQm.Services.SearchConstraints;
+
+    [TestClass]
+    public class ОтTests
+    {
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void DesiredUsernameCantBeNull()
+        {
+            new От(null);
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void DesiredUsernameCantBeWhitespace()
+        {
+            new От("              ");
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void AllowingRecipeWithAuthorWithDesiredUsername()
+        {
+            var constraint = new От("иван");
+            var recipe = new Recipe()
+                         {
+                             Author = new ApplicationUser()
+                                      {
+                                          UserName = "иван"
+                                      }
+                         };
+            Assert.IsTrue(constraint.IsAllowed(recipe));
+        }
+
+        [TestMethod]
+        public void AllowingRecipeWithAuthorWithDesiredUsername2()
+        {
+            var constraint = new От("Ванката         ");
+            var recipe = new Recipe()
+                         {
+                             Author = new ApplicationUser()
+                                      {
+                                          UserName = "Ванката"
+                                      }
+                         };
+            Assert.IsTrue(constraint.IsAllowed(recipe));
+        }
+
+        [TestMethod]
+        public void DenyRecipeWithAuthorWithDifferentThatDesiredUsername()
+        {
+            var constraint = new От("Иван");
+            var recipe = new Recipe()
+                         {
+                             Author = new ApplicationUser()
+                                      {
+                                          UserName = "Георги"
+                                      }
+                         };
+            Assert.IsFalse(constraint.IsAllowed(recipe));
+        }
+    }
+}
