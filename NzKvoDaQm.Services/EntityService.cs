a2a0e447ed586cf974c3682e9d13c74b223c2b7a
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-04-10 20:29:35 +0300

    Base class for services manipulating entities

---
+++ NzKvoDaQm.Services/EntityService.cs
@@ -0,0 +1,49 @@
+﻿namespace NzKvoDaQm.Services
+{
+
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    using NzKvoDaQm.Services.Interfaces;
+
+    public abstract class EntityService<T> : IEntityService<T> where T : class
+    {
+        private readonly IDbSet<T> set;
+
+        protected EntityService(IDbSet<T> set)
+        {
+            if (set == null)
+            {
+                throw new ArgumentNullException(nameof(set));
+            }
+
+            this.set = set;
+        }
+
+        public T Get(long id)
+        {
+            return this.set.Find(id);
+        }
+
+        public IQueryable<T> Get(Func<T, bool> condition)
+        {
+            return this.set.Where(condition).AsQueryable();
+        }
+
+        public bool Delete(T entity)
+        {
+            try
+            {
+                this.set.Remove(entity);
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+}
---
+++ NzKvoDaQm.Services/Interfaces/IEntityService.cs
@@ -0,0 +1,15 @@
+﻿namespace NzKvoDaQm.Services.Interfaces
+{
+
+    using System;
+    using System.Linq;
+
+    public interface IEntityService<T>
+    {
+        T Get(long id);
+
+        IQueryable<T> Get(Func<T, bool> condition);
+
+        bool Delete(T entity);
+    }
+}
