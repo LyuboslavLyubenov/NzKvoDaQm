ee2467d9f3f3ba840960add12456eeecd9301cf7
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-04-11 19:03:04 +0300

    Convert base64 string to Image object

---
+++ NzKvoDaQm.Services/Utils/ImageUtils.cs
@@ -0,0 +1,23 @@
+﻿namespace NzKvoDaQm.Services.Utils
+{
+
+    using System;
+    using System.Drawing;
+    using System.Drawing.Imaging;
+    using System.IO;
+    using System.Text;
+
+    public class ImageUtils
+    {
+        ImageUtils()
+        {   
+        }
+
+        public static Image ConvertBase64ToImage(string base64)
+        {
+            var bytes = Convert.FromBase64String(base64);
+            var stream = new MemoryStream(bytes);
+            return Bitmap.FromStream(stream);
+        }
+    }
+}
